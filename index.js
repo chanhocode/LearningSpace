1a07f0de9f3a4a351f2d3051f778fa7e1859f746
Chanho <chjo1245@gmail.com>
2022-07-01 22:40:07 +0900

    백엔드 주요 기능 구현

--- index.js
+++ index.js
@@ -1,36 +1,82 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const port = 5000;
 
-const config = require('./config/key')
+const config = require("./config/key");
 
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
+
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    config.mongoURI
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDb Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) =>
-  res.send("Hello World!! _  안녕하세요~")
-);
+app.get("/", (req, res) => res.send("Hello World!! _  안녕하세요~"));
 
-app.post("/register", (req, res) => {
-  // 필요 정보 데이터 베이스 삽입
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    console.log("test");
+    return res.status(200).json({
+      sucess: true,
+    });
+  });
+});
 
-  user.save((err) => {
-    if (err) {
-      return res.json({ success: false, err });
-    } else {
-      return res.status(200), json({ success: true });
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Unvalid email",
+      });
     }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: falsse,
+          message: "Wrong password",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
   });
 });
 
