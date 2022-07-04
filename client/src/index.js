433585177b28a17e35ee2a453cce4664e42a6b08
Chanho <chjo1245@gmail.com>
2022-07-04 15:21:52 +0900

    Redux setting

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+// commet:: default 키워드를 이용해 내보냈으므로 다른이름으로 명시 해서 가져올 수 있다.
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
