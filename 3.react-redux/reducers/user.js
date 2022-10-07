75b3e39b8b32bb30c1cd758d520f61f059f851fd
chanho <chjo1245@gmail.com>
2022-10-07 16:02:26 +0900

    Complete: 4.react-redux-immer

--- 3.react-redux/reducers/user.js
+++ 3.react-redux/reducers/user.js
@@ -3,27 +3,34 @@ const initialState = {
   data: null,
 };
 
-const userReducer = (prevState = initialState, action) => { // 새로운 state 만들어주기
+// 추후 유지보수를 위해 상수로 변수를 저장.
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const LOG_OUT = 'LOG_OUT';
+
+const userReducer = (prevState = initialState, action) => {
+  // 새로운 state 만들어주기
   switch (action.type) {
-    case 'LOG_IN_REQUEST':
+    case LOG_IN_REQUEST:
       return {
         ...prevState,
         data: null,
         isLoggingIn: true,
       };
-    case 'LOG_IN_SUCCESS':
+    case LOG_IN_SUCCESS:
       return {
         ...prevState,
         data: action.data,
         isLoggingIn: false,
       };
-    case 'LOG_IN_FAILURE':
+    case LOG_IN_FAILURE:
       return {
         ...prevState,
         data: null,
         isLoggingIn: false,
       };
-    case 'LOG_OUT':
+    case LOG_OUT:
       return {
         ...prevState,
         data: null,
