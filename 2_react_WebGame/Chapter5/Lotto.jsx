11f7888b49c4c6f122ef40a75465e380f80e1b2b
“ChanhoJO” <chjo1245@gmail.com>
2023-01-30 21:57:32 +0900

    Learn: Chapter5

---
+++ 2_react_WebGame/Chapter5/Lotto.jsx
@@ -0,0 +1,98 @@
+import react, { Component } from 'react';
+import Ball from './Ball';
+
+function getWinNumbers() {
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonuseNumber = shuffle[shuffle.length - 1];
+  const winNumber = shuffle.splice(0, 6).sort((p, c) => p - c);
+  return [...winNumber, bonuseNumber];
+}
+
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(), // 당첨 숫자들
+    winBalls: [],
+    bonus: null, // 보너스 공
+    redo: false,
+  };
+
+  timeouts = [];
+
+  runTimeouts = () => {
+    console.log('runTimeouts');
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeouts();
+    console.log('number create');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
+    if (prevState.winNumbers !== this.state.winNumbers) {
+      console.log('number create');
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo = () => {
+    console.log('onClickRedo');
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [],
+      bonus: null, // 보너스 공
+      redo: false,
+    });
+    this.timeouts = [];
+  };
+
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id='결과창'>
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
+      </>
+    );
+  }
+}
+
+export default Lotto;
---
+++ 2_react_WebGame/Chapter5/client.jsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import reactDom from 'react-dom';
+
+import Lotto from './Lotto';
+
+reactDom.render(<Lotto />, document.querySelector('#root'));
