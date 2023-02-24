211fb9a394188fc067e78c6d9cf0a6079f267217
“chanhocode” <chjo1245@gmail.com>
2023-02-24 20:14:50 +0900

    Complete: 6.Mongoose

---
+++ 6.Mongoose/schemas/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const { Types: { ObjectId } } = Schema;
+const commentSchema = new Schema({
+  commenter: {
+    type: ObjectId,
+    required: true,
+    ref: 'User', // user 스키마의 _id를 가리킨다. (JOIN과 같은 기능)
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
