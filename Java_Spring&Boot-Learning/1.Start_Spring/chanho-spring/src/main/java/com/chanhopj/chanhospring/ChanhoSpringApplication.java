660767bd0eaba8eca74387bb74725ab8c7d0efe7
chanhocode <chjo1245@gmail.com>
2023-04-28 01:43:27 +0900

    SpringBoot: 웹개발 기초 실습

---
+++ Java_Spring&Boot-Learning/1.Start_Spring/chanho-spring/src/main/java/com/chanhopj/chanhospring/ChanhoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.chanhopj.chanhospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanhoSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChanhoSpringApplication.class, args);
+	}
+
+}
---
+++ Java_Spring&Boot-Learning/1.Start_Spring/chanho-spring/src/test/java/com/chanhopj/chanhospring/ChanhoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanhopj.chanhospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanhoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
