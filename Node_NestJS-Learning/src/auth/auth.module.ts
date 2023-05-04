31213d73e33ffc862b25aec2f1ea4c1a4cce86d2
chanhocode <chjo1245@gmail.com>
2023-05-05 01:08:39 +0900

    Feat: 유저 유효성 체크 및 비밀번호 암호화 하기

--- Node_NestJS-Learning/src/auth/auth.module.ts
+++ Node_NestJS-Learning/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { TypeOrmExModule } from '../customDecorator/typeorm-custom';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
