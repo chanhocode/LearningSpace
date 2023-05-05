0bca722f84076cc8c48be945184a7cb696c603f9
chanhocode <chjo1245@gmail.com>
2023-05-05 18:10:10 +0900

    Feat: jwt token 생성

--- Node_NestJS-Learning/src/auth/auth.module.ts
+++ Node_NestJS-Learning/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { TypeOrmExModule } from '../customDecorator/typeorm-custom';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secretcode',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- Node_NestJS-Learning/src/auth/auth.service.ts
+++ Node_NestJS-Learning/src/auth/auth.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.findOne({
+      where: {
+        username,
+      },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // 토큰 생성 ( Secret + Payload )
+      const payload = { username }; // 중요정보는 X
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
   }
 }
