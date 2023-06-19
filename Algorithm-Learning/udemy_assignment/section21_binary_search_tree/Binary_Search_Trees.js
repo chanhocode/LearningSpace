eab5c3d7fcea8b1dcca508f1b1ef29d7025382b7
chanhocode <chjo1245@gmail.com>
2023-06-19 16:54:54 +0900

    Algorithm_u_binarySearchTrees

---
+++ Algorithm-Learning/udemy_assignment/section21_binary_search_tree/Binary_Search_Trees.js
@@ -0,0 +1,57 @@
+// Binary Search Trees
+// 왼쪽노드는 항상 작고, 오른쪽 노드는 항상 크다.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (ture) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ Algorithm-Learning/udemy_assignment/section21_binary_search_tree/readme.md
@@ -0,0 +1,18 @@
+## Trees
+
+- 노드로 이루어진 데이터 구조
+- 노드들 사이에 부모-자식 관계가 있다.
+- 비선형구조
+- 관련 용어
+  - Root: 트리 꼭대기 노드 (최상위 노드)
+  - Child: 루트에서 멀어지는 방향으로 연결된 노드 (하위 노드)
+  - Parent: 자식의 반대 (상위 노드)
+  - Siblings: 같은 부모를 가지는 노드
+  - Leaf: 자식이 없는 노드 (단말 노드)
+  - Edge: 간선 \_ 한노드에서 다른 노드로 향하는 화살표
+
+## Kinds of Trees
+
+- Trees
+- Binary Trees
+- Binary Search Trees
