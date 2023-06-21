e98608c8a14d13f7e36ecbea9a51a398b1ef67e2
chanhocode <chjo1245@gmail.com>
2023-06-21 17:44:48 +0900

    Algorithm_u_heaps

---
+++ Algorithm-Learning/udemy_assignment/section23_heap/Heaps.js
@@ -0,0 +1,69 @@
+// Heaps
+// Max Binary Heap: 부모가 언제나 자식보다 크다. 형제 노드 사이에서는 그런 내용이 보장 되지 않는다.
+// Min Binary Heap: 자식은 부모보다 커야한다.
+
+// 노드위치 찾기 _ 자식위치: 2n + 1, 부모위치: (n-1)/2 (소수는 내림)
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+}
