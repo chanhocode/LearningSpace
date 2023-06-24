b8a7ad40d7cb4854f5dc092ab98033c1c16a1a93
chanhocode <chjo1245@gmail.com>
2023-06-24 23:24:30 +0900

    Algorithm_u_Graphs Info

---
+++ Algorithm-Learning/udemy_assignment/section25_graphs/graphs.js
@@ -0,0 +1,21 @@
+// Graphs
+
+/**
+
+Graphs: 노드와 각 꼭지점의 연결쌍의 집합 (Nodes + Connections)
+
+# 그래프의 이용
+- Social Networks
+- Location/Mapping
+- Routing Algorithms
+- Visual Hierarchy
+- File System Optimizations
+- ...
+
+# Essential Graph Terms
+- Vertex: a node
+- Edge: Connection between nodes
+- Weighted/Unweighted: values assigned to distances between vertices
+- Directed/Undirected: direcions assigned to distanced between vertices
+
+ */
