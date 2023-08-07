ee28fb1f73f79e695f011c67ec59d8fe690109b1
chanhocode <chjo1245@gmail.com>
2023-08-07 12:11:58 +0900

    Lean The SpringBoot _ date: 230807

---
+++ Java_SpringBoot_LearnTheBasics/LTB-2-3/src/test/java/com/ltb23/ltb23/Ltb23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltb23.ltb23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltb23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
