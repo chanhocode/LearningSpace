be1d3b30805325fc80ebd29c561f6e0f51dda97b
chanhocode <chjo1245@gmail.com>
2023-10-04 23:36:43 +0900

    Algorithm

--- Algorithm-Learning/Programmers/Coding_test_lv0/lv0_apply_gcd.js
+++ Algorithm-Learning/Programmers/Coding_test_lv0/lv0_apply_gcd.js
@@ -12,17 +12,19 @@ function solution(denum1, num1, denum2, num2) {
 }
 
 // test code
-solution(1,2,3,4) // [5,4]
+solution(1, 2, 3, 4); // [5,4]
 
 // 인상 깊은 코드
-function fnGCD(a, b){
-  return (a%b)? fnGCD(b, a%b) : b;
+function fnGCD(a, b) {
+  return a % b ? fnGCD(b, a % b) : b;
 }
 
 function solution(denum1, num1, denum2, num2) {
-  let denum = denum1*num2 + denum2*num1;
+  let denum = denum1 * num2 + denum2 * num1;
   let num = num1 * num2;
   let gcd = fnGCD(denum, num); //최대공약수
 
-  return [denum/gcd, num/gcd];
+  return [denum / gcd, num / gcd];
 }
+
+// check
