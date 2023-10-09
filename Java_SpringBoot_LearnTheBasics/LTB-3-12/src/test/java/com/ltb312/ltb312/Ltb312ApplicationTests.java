dcb80f7bcd297ab599f1f04770dfc8f0eddc7e50
chanhocode <chjo1245@gmail.com>
2023-10-09 21:46:21 +0900

    Lean The SpringBoot_actuator

---
+++ Java_SpringBoot_LearnTheBasics/LTB-3-12/src/test/java/com/ltb312/ltb312/Ltb312ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltb312.ltb312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltb312ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
