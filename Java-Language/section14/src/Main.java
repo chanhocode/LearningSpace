591dcd665fdf406383c06718121de667a0a9ef2f
chanhocode <chjo1245@gmail.com>
2023-11-30 23:01:59 +0900

    Java language chapter14_jar

---
+++ Java-Language/section14/src/Main.java
@@ -0,0 +1,9 @@
+import com.petshop.Dog;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                new Dog("멍멍이").makeSound();
+        );
+    }
+}
---
+++ Java-Language/section14_java-jar/src/com/petshop/Main.java
@@ -0,0 +1,11 @@
+package com.petshop;
+
+public class Main {
+    public static void main(String[] args) {
+        Pet.intro();
+        Pet cat = new Cat("야옹이");
+        Pet dog = new Dog("멍멍이");
+        System.out.println(cat.makeSound());
+        System.out.println(dog.makeSound());
+    }
+}
