354252a46b6572888205c4e73edd2dcddefaed31
chanhocode <chjo1245@gmail.com>
2023-12-04 00:54:34 +0900

    JPQL_SELECT - #4

--- Java_JAP_ORM_Learning/Hello_JPQL/src/main/java/jpql/JpaMain.java
+++ Java_JAP_ORM_Learning/Hello_JPQL/src/main/java/jpql/JpaMain.java
@@ -16,26 +16,15 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
-            // (query, type)
-            // TypeQuery: 반환 타입이 명확할 때 사용.
-            // Query: 반환 타입이 명확하지 않을 때 사용.
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            Query query2 = em.createQuery("select m.username, m.age from Member m");
-            List<Member> result = query.getResultList();
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
 
-            // 값이 하나인경우 (where~)
-            // query.getSingleResult();
+            em.flush();
+            em.clear();
 
-            // 바인딩
-            TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query3.setParameter("username", "mamber1");
-            Member singleResult = query3.getSingleResult();
-            System.out.println("singleResult: " + singleResult);
-
-
+            List<MemberDTO> query = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                            .getResultList();
+            MemberDTO memberDTO = query.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
 
             tx.commit();
         } catch(Exception e) {
