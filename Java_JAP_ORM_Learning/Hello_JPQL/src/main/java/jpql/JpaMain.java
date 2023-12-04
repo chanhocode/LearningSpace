0d439816b77d96db8475da89a504f8f40891ba18
chanhocode <chjo1245@gmail.com>
2023-12-04 19:09:16 +0900

    JPQL_jon & subquery - #6

--- Java_JAP_ORM_Learning/Hello_JPQL/src/main/java/jpql/JpaMain.java
+++ Java_JAP_ORM_Learning/Hello_JPQL/src/main/java/jpql/JpaMain.java
@@ -12,24 +12,26 @@ public class JpaMain {
         tx.begin();
 
         try{
-            for(int i = 0; i< 100;i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            List<Member> result = em.createQuery(query, Member.class)
                     .setFirstResult(1)
                     .setMaxResults(10)
                     .getResultList();
-            System.out.println("result.size : " + result.size());
-            for(Member m1:result) {
-                System.out.println("m1: " + m1);
-            }
+            tx.commit();
 
         } catch(Exception e) {
             tx.rollback();
