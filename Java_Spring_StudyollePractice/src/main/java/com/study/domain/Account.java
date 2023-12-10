6bc72a172ff2804d69375dfa73fde43bdb3f0aaa
chanhocode <chjo1245@gmail.com>
2023-12-10 16:45:39 +0900

    Studyolle: 계정 도메인 생성 #9

---
+++ Java_Spring_StudyollePractice/src/main/java/com/study/domain/Account.java
@@ -0,0 +1,49 @@
+package com.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+    private String password;
+
+    // 이메일 인증(계정) 유무 판단
+    private boolean emailVerified;
+    // 이메인 검증 토큰 값
+    private String emailCheckToken;
+
+    // 가입 날짜 (이메일 인증 시점)
+    private LocalDateTime joinedAt;
+
+    // Profile
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    // Alarm
+    // 스터디가 만들어졌음을 이메일, 웹 으로 알림 받을 것인지
+    private boolean studyCreateByEmail;
+    private boolean studyCreateByWeb;
+
+    // 가입 신청 결과를 이메일, 웹 으로 알림 받을 것인지
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+
+    // 스터디에 대한 갱신된 정보들을 이메일, 웹 으로 받을 것인지
+    private boolean studyUpdatedByEmail;
+    private boolean studyUpdatedByWeb;
+}
