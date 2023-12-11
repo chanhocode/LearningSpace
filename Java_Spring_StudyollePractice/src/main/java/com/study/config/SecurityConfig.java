ede873e383deddbbf17c037fca5845801d946a5b
chanhocode <chjo1245@gmail.com>
2023-12-11 23:28:00 +0900

    Studyolle: 회원가입 뷰 구현 #11

--- Java_Spring_StudyollePractice/src/main/java/com/study/config/SecurityConfig.java
+++ Java_Spring_StudyollePractice/src/main/java/com/study/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.study.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +24,11 @@ public class SecurityConfig{
                     authorizeRequests.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
