aef902317a060136885486a23b8b75e65897f21c
chanhocode <chjo1245@gmail.com>
2023-12-17 23:00:39 +0900

    Studyolle: 관심주제 WhiteList 기능 추가

--- Java_Spring_StudyollePractice/src/main/java/com/study/Settings/SettingsController.java
+++ Java_Spring_StudyollePractice/src/main/java/com/study/Settings/SettingsController.java
@@ -1,5 +1,7 @@
 package com.study.Settings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.Settings.form.*;
 import com.study.Settings.validator.NicknameValidator;
 import com.study.Settings.validator.PasswordFormValidator;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -49,6 +52,7 @@ public class SettingsController {
     private final ModelMapper modelMapper;
     private final NicknameValidator nicknameValidator;
     private final TagRepository tagRepository;
+    private final ObjectMapper objectMapper;
 
     @GetMapping(SETTINGS_PROFILE_URL)
     public String updateProfileForm(@CurrentUser Account account, Model model) {
@@ -128,10 +132,13 @@ public class SettingsController {
     }
 
     @GetMapping(SETTINGS_TAGS_URL)
-    public String updateTags(@CurrentUser Account account, Model model) {
+    public String updateTags(@CurrentUser Account account, Model model) throws JsonProcessingException {
         model.addAttribute(account);
         Set<Tag> tags = accountService.getTages(account);
         model.addAttribute("tags", tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
+
+        List<String> allTags = tagRepository.findAll().stream().map(Tag::getTitle).collect(Collectors.toList());
+        model.addAttribute("whitelist", objectMapper.writeValueAsString(allTags));
         return  SETTINGS_TAGS_VIEW_NAME;
     }
 
