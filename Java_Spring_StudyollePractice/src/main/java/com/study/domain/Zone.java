71d304c84c526ec7750513ad4e738812c6ac25bc
chanhocode <chjo1245@gmail.com>
2023-12-18 01:53:25 +0900

    Studyolle: 지역 도메인 #25

---
+++ Java_Spring_StudyollePractice/src/main/java/com/study/domain/Zone.java
@@ -0,0 +1,25 @@
+package com.study.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Zone {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String localNameOfCity;
+
+    @Column(nullable = true)
+    private String province;
+}
---
+++ Java_Spring_StudyollePractice/src/main/java/com/study/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.study.zone;
+
+import com.study.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
