d87eb42bf74385474a2e45cae88285aa816ca1c6
chanhocode <chjo1245@gmail.com>
2024-02-09 19:39:58 +0900

    2024-02-09: STS4 셋팅 및 프로젝트 생성

---
+++ Java_Spring_Master_Package/Learn-Spring-Frameword-Start/src/test/java/com/app/LearnSpringFramewordStart/LearnSpringFramewordStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.LearnSpringFramewordStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFramewordStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
