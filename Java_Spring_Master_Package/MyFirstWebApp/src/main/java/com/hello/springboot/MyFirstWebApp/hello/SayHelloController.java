1e35aadec808b0cff4b33584c3f83ab1b34d5e2c
chanhocode <chjo1245@gmail.com>
2024-02-17 01:48:34 +0900

    2024-12-16: Spring master section7 Project 생성 및 기본 설정

---
+++ Java_Spring_Master_Package/MyFirstWebApp/src/main/java/com/hello/springboot/MyFirstWebApp/hello/SayHelloController.java
@@ -0,0 +1,31 @@
+package com.hello.springboot.MyFirstWebApp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	// "say-hello" => Hello What are you learning today?
+	// http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>My first Page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("<div>html page with body</div>");
+		sb.append("</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
+
+}
