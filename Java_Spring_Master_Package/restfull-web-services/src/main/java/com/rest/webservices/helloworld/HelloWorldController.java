4ab6603244e39af6de80fc7260d4383b1b10ab56
chanhocode <chjo1245@gmail.com>
2024-02-26 00:55:53 +0900

    2024-02-25: spring master section8 실습

--- Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.rest.webservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	// /hello-world
 	@GetMapping("/hello-world")
@@ -23,5 +33,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
--- Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/user/UserResource.java
+++ Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/user/UserResource.java
@@ -1,13 +1,23 @@
 package com.rest.webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -25,15 +35,30 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{userId}")
-	public User retrieveUser(@PathVariable Integer userId) {
-		return service.findOne(userId);
+	public EntityModel<User> retrieveUser(@PathVariable Integer userId) {
+		User user = service.findOne(userId);
+		if (user== null) {
+			throw new UserNotFoundException("id: " + userId);
+		}
+		
+		EntityModel<User> entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
 		
-		return ResponseEntity.created(null).build();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public void deleteUser(@PathVariable Integer userId) {
+		service.deleteById(userId);
 	}
 	
 }
