08ecfcaf815ee0f07f47c1c0df8438227b47f094
chanhocode <chjo1245@gmail.com>
2024-03-02 00:25:46 +0900

    2024-03-02: git test

--- Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/security/SpringSecurityConfiguration.java
+++ Java_Spring_Master_Package/restfull-web-services/src/main/java/com/rest/webservices/security/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 설정 
 @Configuration
 public class SpringSecurityConfiguration {
 	
