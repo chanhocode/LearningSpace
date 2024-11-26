f271c2e54af7110e0ca6ec5ee58f06049fba7ddb
chanhocode <chjo1245@gmail.com>
2024-11-26 22:19:26 +0900

    onebite-ts: 2024-11-26 TS Config

--- onebite_ts/section1/src/index.ts
+++ onebite_ts/section1/src/index.ts
@@ -1,2 +1,9 @@
-console.log('hello');
-const a: number = 1;
+// import { func } from './test';
+// func('hi');
+
+// 블록 범위 변수를 다시 선언할 수 없습니다.
+const a = 1;
+// 타입스크립트는 파일들을 전역 모듈로 보기 때문에 이러한 문제가 발생합니다.
+// 해결 방법(1)
+// export {};
+// 해결 방법(2): 옵션사용 moduleDetection
---
+++ onebite_ts/section1/src/test.ts
@@ -0,0 +1,4 @@
+// export const func = (message: string) => console.log('hello' + message);
+
+// 블록 범위 변수를 다시 선언할 수 없습니다.
+const a = 1;
